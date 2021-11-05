f678e1135c7604bfd4dda2dc790e3f136b2732f3
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-06 00:26:22 +0900

    Modified: 컨벤션 수정, 새로운 컴포넌트에 scss 추가

--- src/pages/Detail/DetailInfo/DetailAccordions/DetailAccordions.js
+++ src/pages/Detail/DetailInfo/DetailAccordions/DetailAccordions.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './DetailAccordions.scss';
 
 export class DetailAccordions extends Component {
   render() {
     return (
-      <>
+      <div className="DetailAccordions">
         <div className="uk-accordion">
           <h2>고객안내</h2>
         </div>
@@ -16,7 +17,7 @@ export class DetailAccordions extends Component {
         <div className="uk-accordion">
           <h2>반품/AS</h2>
         </div>
-      </>
+      </div>
     );
   }
 }
