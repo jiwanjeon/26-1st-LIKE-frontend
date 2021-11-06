4f910bf760d8204bba911b31224a04a846d2f984
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-06 20:56:27 +0900

    Modified: 컨벤션 수정 및 MiniCartModal.scss 추가

--- src/pages/Detail/MiniCartModal/MiniCartModal.js
+++ src/pages/Detail/MiniCartModal/MiniCartModal.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import MiniCart from './MiniCart';
+import './MiniCartModal.scss';
 
 export class MiniCartModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
+      open: false,
     };
   }
 
@@ -33,10 +34,10 @@ export class MiniCartModal extends Component {
     const { open } = this.state;
     return (
       open && (
-        <>
+        <div className="MiniCartModal">
           <MiniCart />
           <div onClick={this.close} className="miniCartOverlay" />
-        </>
+        </div>
       )
     );
   }
