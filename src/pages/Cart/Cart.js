842d4966f86e2aad6b473f92b933cd8c44ea6693
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-08 20:12:52 +0900

    Add: 장바구니 페이지 레이아웃

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -13,7 +13,12 @@ export class Cart extends Component {
             <div className="cartNumber">
               <span>1개 상품</span>
             </div>
-            <div className="cartContainer" />
+            <div className="cartContainer">
+              사용 가능한 쿠폰이 있습니다
+              <br />
+              아래 프로모 코드란에 입력하여 사용해주세요
+              <div className="myPageCart">hi</div>
+            </div>
           </main>
         </div>
       </div>
