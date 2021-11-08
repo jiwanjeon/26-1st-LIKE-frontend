1595a0243f75124fab17edd959789243f21df237
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-08 11:45:10 +0900

    Modified: 레이아웃 초안 작업

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Nav from '../../components/Nav/Nav';
+import './Cart.scss';
 
 export class Cart extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <div className="Cart">
+          <main className="cartInner">
+            <h2 className="cartTitle">HI</h2>
+            <div className="cartNumber">
+              <span>1개 상품</span>
+            </div>
+            <div className="cartContainer" />
+          </main>
+        </div>
       </div>
     );
   }
