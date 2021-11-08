64fdd030d76f897937266a7847ee3f41518ffc8f
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-08 11:19:45 +0900

    Add: Cart.js과  라우터 할당

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from '../../components/Nav/Nav';
+
+export class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
+  }
+}
+
+export default Cart;
