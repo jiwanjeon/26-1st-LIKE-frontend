8b3d76970378b3ab0b230e4fc1513723aebbd729
newjinny <syj9437@gmail.com>
2021-11-10 20:19:53 +0900

    Add:로그인 유효성 검사 완료

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,13 +3,30 @@ import './Login.scss';
 import { Link } from 'react-router-dom';
 
 export class Login extends Component {
+  nn;
   constructor() {
     super();
     this.state = {
       id: '',
       password: '',
+      checkemail: false,
+      checkpassword: false,
     };
   }
+
+  checkingemail = e => {
+    const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-_]+$/;
+    this.setState({
+      checkemail: emailRegex.test(e.target.value),
+    });
+  };
+
+  checkingpassword = e => {
+    const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+    this.setState({
+      checkpassword: pwdRegex.test(e.target.value),
+    });
+  };
 
   handleIdInput = e => {
     const { value } = e.target;
@@ -37,7 +54,10 @@ export class Login extends Component {
   };
 
   render() {
+    console.log(this.state.checkemail);
+    console.log(this.state.checkpassword);
     const { id, password } = this.state;
+
     return (
       <section className="Login">
         <img className="Logo" alt="LogoLike" src="/images/LogoLIKE.svg" />
@@ -50,7 +70,13 @@ export class Login extends Component {
               placeholder="아이디"
               value={id}
               onChange={this.handleIdInput}
+              onKeyUp={this.checkingemail}
             />
+            {this.state.checkemail ? null : (
+              <div className="checkingEmail">
+                <span>필수 입력 항목입니다.</span>
+              </div>
+            )}
             <input
               className="password"
               type="password"
@@ -58,7 +84,13 @@ export class Login extends Component {
               maxLength="16"
               value={password}
               onChange={this.handlePwInput}
+              onKeyUp={this.checkingpassword}
             />
+            {this.state.checkpassword ? null : (
+              <div className="checkingPwd">
+                <span>필수 입력 항목입니다.</span>
+              </div>
+            )}
           </div>
           <div className="inputBox">
             <div className="checkboxWrap">
@@ -74,6 +106,7 @@ export class Login extends Component {
             </div>
           </div>
           <button
+            className="loginButton"
             disabled={
               this.state.id.includes('@' && '.') &&
               this.state.password.length >= 8 &&
@@ -84,7 +117,7 @@ export class Login extends Component {
             type="submit"
             onClick={this.goToMain}
           >
-            <p>로그인</p>{' '}
+            <p>로그인</p>
           </button>
         </div>
         <div className="last">
